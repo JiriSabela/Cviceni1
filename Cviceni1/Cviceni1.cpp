733e0f036f423fa2a70812196fd3dc0feb01d41f
Student <Student@PC-SE5113-17>
2023-09-21 11:56:19 +0200

    Add project files.

---
+++ Cviceni1/Cviceni1.cpp
@@ -0,0 +1,12 @@
+﻿// Cviceni1.cpp : Defines the entry point for the application.
+//
+
+#include "Cviceni1.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello CMake.");
+	printf("\n");
+	return 0;
+}
