7d15ac914a449b63eed8e307dcb41226b4ae2fa7
Jiří Sabela <145663520+JiriSabela@users.noreply.github.com>
2023-09-21 11:58:24 +0200

    Update Cviceni1.cpp
    Smazání komentářů a editace zprávy

--- Cviceni1/Cviceni1.cpp
+++ Cviceni1/Cviceni1.cpp
@@ -1,12 +1,9 @@
-﻿// Cviceni1.cpp : Defines the entry point for the application.
-//
-
 #include "Cviceni1.h"
 #include <stdio.h>
 
 int main(void)
 {
-	printf("Hello CMake.");
+	printf("Hello Class.");
 	printf("\n");
 	return 0;
 }
